7e90fcbca25d4f5e34ba433995016d4a3bbb566d
Ayush Sahu <ayushsahu29042005@gmail.com>
2025-12-02 18:49:52 +0530

    Delete student-modern-ui-CORRECT directory

--- student-modern-ui-CORRECT/src/services/studentService.js
+++
@@ -1,8 +0,0 @@
-
-const API = "http://localhost:3001/students";
-
-export const getStudents = async () => (await fetch(API)).json();
-export const addStudent = async (s) => (await fetch(API, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(s) })).json();
-export const updateStudent = async (id, s) => (await fetch(API + '/' + id, { method: 'PUT', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(s) })).json();
-export const deleteStudent = async (id) => await fetch(API + '/' + id, { method: 'DELETE' });
-// json-server --watch db.json --port 3001
